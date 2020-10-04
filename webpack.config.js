136a52badf0b69e032902f1a795a14d13cadf2d0
alertguo <1196614612@qq.com>
2020-10-05 00:15:07 +0800

    update config

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,19 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path')
+const base = require('./webpack.config.base.js')
 
 module.exports = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    filename: '[name].[contenthash].js'
-  },
+  ...base,
   devServer: {
     contentBase: './dist',
   },
-  plugins: [new HtmlWebpackPlugin({
-    title: 'webpack',
-    template: 'src/assets/test.html'
-  })],
+  mode: 'development',
   module: {
     rules: [
       {
         test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ['style-loader','css-loader',],
       },
     ],
   },
