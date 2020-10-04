ae0755802a201988467d5087b2323dac78a5d599
alertguo <1196614612@qq.com>
2020-10-04 21:20:08 +0800

    create html

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 var path = require('path')
 
 module.exports = {
@@ -5,5 +7,9 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: '[name].[contenthash].js'
-  }
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: 'webpack',
+    template: 'src/assets/test.html'
+  })]
 }
