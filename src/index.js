24283550ef80c60b3e818c8293114f88fec210fb
alertguo <1196614612@qq.com>
2020-10-05 20:38:15 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ console.log(x)
 import png from './assets/1.png'
 console.log(png)
 const div = document.getElementById('app')
+console.log(div)
 div.innerHTML = `
   <img src="${png}">
 `
+
+const button = document.createElement('button')
+button.innerText = '懒加载'
+button.onclick = ()=>{
+  const promise = import('./lazy.js')
+  promise.then((module)=>{
+    const fn = module.default
+    fn()
+  },()=>{console.log('模块加载错误')})
+}
+div.appendChild(button)
